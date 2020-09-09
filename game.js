66a42fa209da335d5b6dbf610cf72d98b1a02041
Ziiphox <ziiphox@gmail.com>
2020-09-09 23:44:03 +1000

    cleaned up the code a bit, mostly working on a basic web hosting solution

--- game.js
+++ game.js
@@ -1,12 +1,18 @@
+function randint() {
+	console.error("Don't use random numbers >:(")
+	process.exit(1)
+}
+
+
+
+
+// main game ting
+function game() {
+	console.log("haha yes")
+}
+
+
 // loads after css
 window.addEventListener("load", (event) => {
 	game();
 });
-
-
-
-
-
-function game() {
-	console.log("haha yes")
-}
--- page.js
+++ page.js
@@ -1,16 +1,28 @@
+
+function constructbuttons(buttonnames) {
+	buttons = ""
+	for (index of buttonnames) {
+		if (typeof index == "string") {
+			buttons+= `<button class="headbutton">${index}</button>`;
+		}
+	lastindex = index;
+	}
+	document.getElementsByClassName("buttoncontainer")[0].innerHTML = buttons
+}
+
+
+
+
+
+
+
+
+
 // very first loading stage, before images or even css
 document.addEventListener("DOMContentLoaded", (event) => {
-	
+	constructbuttons(["placeholder", "lorem", "ipsum", "dolor", "sit"]);
 });
-
 
 // loads after css
 window.addEventListener("load", (event) => {
-	const buttonnames = ["placeholder", "lorem", "ipsum", "dolor", "sit"]
-	buttons = ""
-	for (index of buttonnames) {
-		buttons+= `<button class="headbutton">${index}</button>`
-	}
-	document.getElementsByClassName("buttoncontainer")[0].innerHTML = buttons
-	
 });
